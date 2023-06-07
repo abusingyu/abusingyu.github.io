9110c2b64b7291e591b2ba974e36d1ad20981ff6
abusedyou <126427990+abusingyu@users.noreply.github.com>
2023-06-07 17:47:58 +0100

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -10,19 +10,19 @@ $(document).ready(() => {
   const links = [
     {
       name: 'my discord',
-      link: 'discord.com/users/745631824163766412',
+      link: 'oiz#4090',
     },
     {
-      name: 'spotify',
-      link: 'open.spotify.com/user/31v42o74y7gq2krt6byixmiboebe?si=f4cd8992f55d42ab',
+      name: '#1',
+      link: 'e fucker',
     },
     {
-      name: 'feds.lol',
-      link: 'feds.lol/ceo',
+      name: 'wanted.lol',
+      link: 'wanted.lol/ishowspeed',
     },
        {
-      name: 'last.fm',
-      link: 'www.last.fm/user/zxqq',
+      name: 'discord',
+      link: 'discord.gg/meekz',
     }
   ];
 
@@ -42,7 +42,7 @@ $(document).ready(() => {
     app.shouldIgnoreVideo = true;
   }
 
-  app.titleChanger(['z', 'zu', 'zur', 'zurl', 'zurly', 'zurly $']);
+  app.titleChanger(['i', 's', 'how', 'spee', 'd', 'dd$']);
   app.iconChanger(['assets/icons/rose1.jpg', 'assets/icons/rose2.jpg', 'assets/icons/rose3.jpg', 'assets/icons/rose4.jpg', 'assets/icons/rose5.jpg', 'assets/icons/rose6.jpg', 'assets/icons/rose7.jpg', 'assets/icons/rose7.jpg', 'assets/icons/rose1.jpg']);
 });
 
